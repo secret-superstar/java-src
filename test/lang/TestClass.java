9cef5deec6de1323488dd2b1622c372f27d0a05d
phil123 <shupan@kuaizi.co>
2020-01-12 00:27:05 +0800

    + add class

--- test/lang/TestClass.java
+++ test/lang/TestClass.java
@@ -2,13 +2,16 @@ package lang;
 
 import org.junit.Test;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * 类的创建和处理操作
  * 描述：
  * 1. 类是通过类的加载器来实现其的操作
- * 2.
+ *
+ * @todo 对于类的深入的研究和处理，可以扩展类的处理特性
  */
 public class TestClass {
 
@@ -21,6 +24,7 @@ public class TestClass {
     @Test
     public void testCrud() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
+        //加载类
         Class testClass = Class.forName("lang.TestClass");
 
         //申请类的对象的处理操作
@@ -28,6 +32,22 @@ public class TestClass {
 
         //可以通过类名获取类的方法擦着哦
         testClass.getMethod("test1").invoke(testClass1);
+
+        //打印类的信息
+        System.out.println(testClass.toString());
+
+        //这里可以对类进行动态的调用，比如通过不同的参数来调用类的玩法
+
+        //循环打印类的信息
+        Class[] interfaces = testClass.getInterfaces();
+
+        Annotation[] annotations = testClass.getAnnotations();
+
+        //获取类下的方法信息
+        Method[] methods = testClass.getMethods();
+
+        //能够获取类的信息，那么就可以获取
+
     }
 
     public void test1() {
